906ba46ed2dee01cb182965bd688736ef3e8b3a8
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-11-29 09:37:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -505,3 +505,562 @@ app.whenReady().then(createwindow)
 //     newapp.loadFile('index.html')
 // }
 // app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
+// const {app,BrowserWindow} = require('electron')
+// createwindow = ()=>
+// {
+//     const newapp = new BrowserWindow({
+//         width: 1000,
+//         height: 800,
+//         resizable: false,
+//         fullscreenable: false,
+//         webPreferences: {nodeIntegration: true}
+//     })
+//     newapp.loadFile('index.html')
+// }
+// app.whenReady().then(createwindow)
